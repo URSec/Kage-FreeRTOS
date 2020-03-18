0a3d8995d9260e19ed2997edda219835a8eed371
Yufei Du <edwarddu1996@gmail.com>
2020-03-18 17:24:12 -0400

    Fixed incorrect offset. Logging works now

--- lib/FreeRTOS/portable/GCC/ARM_CM4_MPU/portmacro.h
+++ lib/FreeRTOS/portable/GCC/ARM_CM4_MPU/portmacro.h
@@ -56,7 +56,7 @@ extern "C" {
 #define portSTACK_SIZE  1024 //256 //128: overflow for fibDyn & print
 #define STACK_SIZE		portSTACK_SIZE
 #define STACK_SIZE_IN_BYTES (STACK_SIZE * sizeof (StackType_t))
-#define STACK_SIZE_IN_BYTES_STATIC	4092
+#define STACK_SIZE_IN_BYTES_STATIC	4096
 #define STACK_SIZE_TAG(X)	#X
 #define STACK_SIZE_INLINE(X) STACK_SIZE_TAG(X)
 
